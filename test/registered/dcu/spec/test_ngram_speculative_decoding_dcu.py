4afe37284dbfe3b4bc5ddc20f1d28b8ad9853504
Lv Jiajia <1187300773@qq.com>
2026-07-05 16:02:29 +0800

    chore: add Apache headers for DCU files

--- test/registered/dcu/spec/test_ngram_speculative_decoding_dcu.py
+++ test/registered/dcu/spec/test_ngram_speculative_decoding_dcu.py
@@ -1,4 +1,18 @@
 # DCU-specific copy of test/registered/spec/test_ngram_speculative_decoding.py.
+# Copyright 2026 Hygon Information Technology Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Changes:
 # - use a local BW1100 model path override
 # - keep only the FA3 paged-attention path supported on DCU
