1e3c875d2c40ae513a29dc126f40c8545779ddee
Lv Jiajia <1187300773@qq.com>
2026-07-07 16:40:29 +0800

    chore: add header for DCU runtime text checker

--- scripts/ci/dcu/check_no_amd_runtime_text.py
+++ scripts/ci/dcu/check_no_amd_runtime_text.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright 2026 Hygon Information Technology Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Fail if HCU-owned changed files add user-visible legacy platform text.
 
 The workflow passes only changed files from the PR or push diff. This script
