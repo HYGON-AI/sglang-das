39d7b8ab177ce24111f11fa431f85d06899bed27
lizhigong <306128847@qq.com>
2026-08-06 08:54:08 +0800

    fix: resolve lint checks for MiniMax RMS quant

--- python/sglang/srt/layers/quantization/w8a8_int8.py
+++ python/sglang/srt/layers/quantization/w8a8_int8.py
@@ -19,6 +19,9 @@ from types import MappingProxyType
 from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Optional, cast
 
 import torch
+
+# from sglang.srt.layers.quantization.int8_kernel import per_token_quant_int8
+from lightop.quant import per_token_quant_int8
 from torch.nn.parameter import Parameter
 
 from sglang.srt.distributed import get_tensor_model_parallel_world_size
@@ -28,6 +31,7 @@ from sglang.srt.layers.amx_utils import (
 )
 from sglang.srt.layers.moe import MoeRunner, MoeRunnerBackend, MoeRunnerConfig
 from sglang.srt.layers.moe.moe_runner.triton import TritonMoeQuantInfo
+from sglang.srt.layers.moe.utils import get_moe_runner_backend
 from sglang.srt.layers.parameter import ChannelQuantScaleParameter, ModelWeightParameter
 from sglang.srt.layers.quantization.base_config import (
     FusedMoEMethodBase,
@@ -35,11 +39,10 @@ from sglang.srt.layers.quantization.base_config import (
     QuantizationConfig,
     QuantizeMethodBase,
 )
-from sglang.srt.layers.moe.utils import get_moe_runner_backend
 from sglang.srt.layers.quantization.compressed_tensors.utils import should_ignore_layer
-# from sglang.srt.layers.quantization.int8_kernel import per_token_quant_int8
-from lightop.quant import per_token_quant_int8
-from sglang.srt.layers.quantization.unquant import UnquantizedLinearMethod, UnquantizedEmbeddingMethod
+from sglang.srt.layers.quantization.unquant import (
+    UnquantizedEmbeddingMethod,
+)
 from sglang.srt.utils import (
     cpu_has_amx_support,
     is_cpu,
@@ -53,6 +56,7 @@ from sglang.srt.utils.patch_torch import register_fake_if_exists
 
 if TYPE_CHECKING:
     from sglang.srt.layers.moe.token_dispatcher import StandardDispatchOutput
+
 from lightop import gemm_ops as quant_ops
 
 _is_cuda = is_cuda()
@@ -62,7 +66,6 @@ _is_cpu = is_cpu()
 _is_cpu_arm64 = is_host_cpu_arm64()
 
 if _is_cuda:
-    from sgl_kernel import int8_scaled_mm
 
     @register_fake_if_exists("sgl_kernel::int8_scaled_mm")
     def _int8_scaled_mm_abstract(
@@ -127,9 +130,10 @@ class W8A8Int8Config(QuantizationConfig):
     ) -> Optional[QuantizeMethodBase]:
         from sglang.srt.layers.linear import LinearBase
         from sglang.srt.layers.moe.fused_moe_triton import FusedMoE
-        from sglang.srt.layers.radix_attention import RadixAttention
         from sglang.srt.layers.quantization.fp8 import Fp8KVCacheMethod
         from sglang.srt.layers.quantization.unquant import UnquantizedFusedMoEMethod
+        from sglang.srt.layers.radix_attention import RadixAttention
+
         if isinstance(layer, LinearBase):
             if should_ignore_layer(
                 prefix, ignore=self.ignore, fused_mapping=self.packed_modules_mapping
