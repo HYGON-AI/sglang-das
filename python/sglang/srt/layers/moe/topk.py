084ae97118fa20c7fa219b662eb9d5f5a6d9b8cf
hnyls2002 <lsyincs@gmail.com>
2026-09-17 22:38:43 -0700

    fix format_is_standard TypeGuard; fix inverted assert message; own TODO

--- python/sglang/srt/layers/moe/topk.py
+++ python/sglang/srt/layers/moe/topk.py
@@ -29,6 +29,7 @@ from typing import (
     Protocol,
     Tuple,
     TypeGuard,
+    Union,
     runtime_checkable,
 )
 
@@ -393,7 +394,9 @@ class TopKConfig:
 
 class TopKOutputChecker:
     @staticmethod
-    def format_is_standard(topk_output: TopKOutput) -> TypeGuard[StandardTopKOutput]:
+    def format_is_standard(
+        topk_output: TopKOutput,
+    ) -> TypeGuard[Union[StandardTopKOutput, StandardTopKOutputPacked]]:
         return isinstance(topk_output, (StandardTopKOutput, StandardTopKOutputPacked))
 
     @staticmethod
