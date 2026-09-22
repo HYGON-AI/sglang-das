becc40a3c24e3353650a307e5efdc6a936c08773
戴仕强 <daishiqiang123@hygon.com>
2026-09-22 17:10:20 +0800

    fix: fix deepgemm w4a8 op scale change

--- python/sglang/srt/layers/quantization/slimquant_w4a8_marlin.py
+++ python/sglang/srt/layers/quantization/slimquant_w4a8_marlin.py
@@ -715,7 +715,7 @@ class SlimQuantW4A8Int8MarlinMoEMethod:
                     pack_w4a8_moe_hipc_weight(layer.w2_weight.data),
                     requires_grad=False,
                 )
-                scale_mul = 16.0
+                scale_mul = 1.0 # 16.0
                 layer.w13_weight_scale = Parameter(
                     layer.w13_weight_scale.data * scale_mul,
                     requires_grad=False,
